c276ff656f48b169f94e9f6bdc1294d2a251a215
Viktor Kotsiuban <gbdrm1@gmail.com>
2018-03-04 21:30:49 +0200

    Add project files.

---
+++ QTF.Web/Models/ManageViewModels/TwoFactorAuthenticationViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QTF.Web.Models.ManageViewModels
+{
+    public class TwoFactorAuthenticationViewModel
+    {
+        public bool HasAuthenticator { get; set; }
+
+        public int RecoveryCodesLeft { get; set; }
+
+        public bool Is2faEnabled { get; set; }
+    }
+}
